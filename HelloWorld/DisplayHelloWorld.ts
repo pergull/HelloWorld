ddb2529195426ff71694179f8a028c90b9df4fce
pergull <pergull@gmail.com>
2020-11-24 21:31:42 -0700

    Update DisplayHelloWorld.ts

--- HelloWorld/DisplayHelloWorld.ts
+++ HelloWorld/DisplayHelloWorld.ts
@@ -1,21 +1,18 @@
-module HelloWorld {
+export class DisplayHelloWorld {
 
-    export class DisplayHelloWorld {
+    constructor() {
+    }
 
-        constructor() {
-        }
+    display() {
+        const canvas = document.createElement("canvas");
+        canvas.width = 500;
+        canvas.height = 300;
+        const ctx = canvas.getContext("2d");
+        ctx.fillStyle = "white";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        document.body.appendChild(canvas);
 
-        display() {
-            const canvas = document.createElement("canvas");
-            canvas.width = 500;
-            canvas.height = 300;
-            const ctx = canvas.getContext("2d");
-            ctx.fillStyle = "white";
-            ctx.fillRect(0, 0, canvas.width, canvas.height);
-            document.body.appendChild(canvas);
-
-            ctx.fillStyle = "#FF0000";
-            ctx.fillText("Hello World", 20, 20);
-        }
+        ctx.fillStyle = "#FF0000";
+        ctx.fillText("Hello World", 20, 20);
     }
 }
